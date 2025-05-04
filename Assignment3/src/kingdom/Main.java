f68df2de84a9229e8672db77b18a73596aa76766
Tutel <jas.lewek@gmail.com>
2025-05-04 20:20:48 +0200

    King & Main

--- Assignment3/src/kingdom/Main.java
+++ Assignment3/src/kingdom/Main.java
@@ -1,5 +1,100 @@
 package kingdom;
 
-public class Main
-{
+import kingdom.actors.*;
+import kingdom.deposit.Deposit;
+import kingdom.treasureroom.TreasureRoom;
+import kingdom.treasureroom.TreasureRoomDoor;
+import kingdom.treasureroom.TreasureRoomGuardsman;
+import kingdom.treasureroom.TreasureRoomReadProxy;
+import kingdom.utils.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    Logger logger = Logger.getInstance();
+    logger.log("Starting the Kingdom simulation");
+
+    // Initialize components
+    TreasureRoom treasureRoom = new TreasureRoom();
+    TreasureRoomDoor guardsman = new TreasureRoomGuardsman(treasureRoom);
+    Deposit deposit = new Deposit(20); // Create deposit with capacity of 20
+
+    // Create actors
+    List<Thread> threads = new ArrayList<>();
+    List<Object> actors = new ArrayList<>();
+
+    // Create miners
+    ValuableMiner miner1 = new ValuableMiner("Miner Bob", deposit, 100, 300, 1000);
+    ValuableMiner miner2 = new ValuableMiner("Miner Jack", deposit, 50, 200, 1500);
+    actors.add(miner1);
+    actors.add(miner2);
+    threads.add(new Thread(miner1));
+    threads.add(new Thread(miner2));
+
+    // Create valuable transporters
+    ValuableTransporter transporter1 = new ValuableTransporter(
+        "Transporter Alpha", deposit, 400, 800, 2000, guardsman);
+    ValuableTransporter transporter2 = new ValuableTransporter(
+        "Transporter Beta", deposit, 300, 600, 2500, guardsman);
+    actors.add(transporter1);
+    actors.add(transporter2);
+    threads.add(new Thread(transporter1));
+    threads.add(new Thread(transporter2));
+
+    // Create king
+    King king = new King("King Arthur", 500, 1200, 300, 3000, 5000, guardsman);
+    actors.add(king);
+    threads.add(new Thread(king));
+
+    // Create accountants
+    TreasureRoomReadProxy readProxy1 = (TreasureRoomReadProxy) guardsman.acquireReadAccess();
+    TreasureRoomReadProxy readProxy2 = (TreasureRoomReadProxy) guardsman.acquireReadAccess();
+
+    Accountant accountant1 = new Accountant("Accountant Smith", readProxy1, 1000, 4000);
+    Accountant accountant2 = new Accountant("Accountant Johnson", readProxy2, 1200, 5000);
+    actors.add(accountant1);
+    actors.add(accountant2);
+    threads.add(new Thread(accountant1));
+    threads.add(new Thread(accountant2));
+
+    // Start all threads
+    logger.log("Starting all actors");
+    for (Thread thread : threads) {
+      thread.start();
+    }
+
+    // Let the simulation run for some time
+    try {
+      Thread.sleep(60000); // Run for 60 seconds
+    } catch (InterruptedException e) {
+      logger.log("Main thread interrupted");
+    }
+
+    // Stop all actors
+    logger.log("Stopping all actors");
+    for (Object actor : actors) {
+      if (actor instanceof ValuableMiner) {
+        ((ValuableMiner) actor).stop();
+      } else if (actor instanceof ValuableTransporter) {
+        ((ValuableTransporter) actor).stop();
+      } else if (actor instanceof King) {
+        ((King) actor).stop();
+      } else if (actor instanceof Accountant) {
+        ((Accountant) actor).stop();
+      }
+    }
+
+    // Wait for all threads to finish
+    try {
+      for (Thread thread : threads) {
+        thread.join(2000);
+      }
+    } catch (InterruptedException e) {
+      logger.log("Main thread interrupted while waiting for actors to stop");
+    }
+
+    logger.log("Kingdom simulation completed");
+  }
 }
--- Assignment3/src/kingdom/actors/King.java
+++ Assignment3/src/kingdom/actors/King.java
@@ -1,5 +1,138 @@
 package kingdom.actors;
 
-public class King
+import kingdom.treasureroom.TreasureRoom;
+import kingdom.treasureroom.TreasureRoomDoor;
+import kingdom.utils.Logger;
+import kingdom.valuables.Valuable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class King implements Runnable
 {
+  private final String name;
+  private final int minPartyValue;
+  private final int maxPartyValue;
+  private final int gatheringTime;
+  private final int partyDuration;
+  private final int restingPeriod;
+  private final TreasureRoomDoor treasureRoomDoor;
+  private final Random random;
+  private volatile boolean running;
+  private final Logger logger;
+
+  public King(String name, int minPartyValue, int maxPartyValue,
+      int gatheringTime, int partyDuration, int restingPeriod,
+      TreasureRoomDoor treasureRoomDoor)
+  {
+    this.name = name;
+    this.minPartyValue = minPartyValue;
+    this.maxPartyValue = maxPartyValue;
+    this.gatheringTime = gatheringTime;
+    this.partyDuration = partyDuration;
+    this.restingPeriod = restingPeriod;
+    this.treasureRoomDoor = treasureRoomDoor;
+    this.random = new Random();
+    this.running = true;
+    this.logger = Logger.getInstance();
+  }
+
+  public void throwParty(int totalValue)
+  {
+    try {
+      logger.logAction(name, "PARTY", "Throwing a lavish party worth " + totalValue);
+      Thread.sleep(partyDuration);
+      logger.logAction(name, "PARTY_OVER", "Party has ended, valuables spent");
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      logger.logAction(name, "INTERRUPTED", "Party was interrupted");
+    }
+  }
+
+  @Override public void run()
+  {
+    logger.logAction(name, "STARTED", "King is ready to throw parties");
+
+    while (running)
+    {
+      try
+      {
+        // Generate random target value for the party
+        int targetValue = random.nextInt(maxPartyValue - minPartyValue + 1) + minPartyValue;
+        logger.logAction(name, "PLANNING", "Planning party with budget of " + targetValue);
+
+        // Acquire write access to the treasure room
+        logger.logAction(name, "REQUESTING", "Requesting access to the treasure room");
+        TreasureRoom treasureRoom = treasureRoomDoor.acquireWriteAccess();
+        logger.logAction(name, "ACCESS", "Gained access to the treasure room");
+
+        // Retrieve valuables
+        List<Valuable> retrievedValuables = new ArrayList<>();
+        int collectedValue = 0;
+        boolean canAffordParty = true;
+
+        while (collectedValue < targetValue)
+        {
+          // Simulate time to select a valuable
+          Thread.sleep(gatheringTime / 10);
+
+          Valuable valuable = treasureRoom.retrieveValuable();
+          if (valuable == null)
+          {
+            logger.logAction(name, "INSUFFICIENT", "Not enough valuables for the party");
+            canAffordParty = false;
+            break;
+          }
+
+          retrievedValuables.add(valuable);
+          collectedValue += valuable.getValue();
+          logger.logAction(name, "RETRIEVED", "Retrieved " + valuable + ", total so far: " + collectedValue);
+        }
+
+        // Check if target was met
+        if (canAffordParty && collectedValue >= targetValue)
+        {
+          logger.logAction(name, "SUCCESS", "Collected enough valuables worth " + collectedValue);
+          treasureRoomDoor.releaseWriteAccess();
+          throwParty(collectedValue);
+        }
+        else
+        {
+          // Return valuables if target not met
+          logger.logAction(name, "RETURNING", "Returning valuables to the treasure room");
+          for (Valuable valuable : retrievedValuables)
+          {
+            Thread.sleep(gatheringTime / 15); // Takes less time to put back
+            treasureRoom.addValuable(valuable);
+            logger.logAction(name, "RETURNED", "Returned " + valuable);
+          }
+          treasureRoomDoor.releaseWriteAccess();
+          logger.logAction(name, "CANCELLED", "Party cancelled");
+        }
+
+        // Rest before planning next party
+        logger.logAction(name, "RESTING", "Resting before next party");
+        Thread.sleep(restingPeriod);
+      }
+      catch (InterruptedException e)
+      {
+        logger.logAction(name, "INTERRUPTED", "King's activities were interrupted");
+        Thread.currentThread().interrupt();
+        break;
+      }
+    }
+
+    logger.logAction(name, "STOPPED", "King is no longer throwing parties");
+  }
+
+  public void stop()
+  {
+    running = false;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
 }
